2579e513004696b69af5215210ffa8a72b3fbd20
Shira Rozental <shira0583241016@gmail.com>
2026-02-16 01:09:24 +0200

    feat: define service layer interfaces

---
+++ Service/Interfaces/IService.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Service.Interfaces
+{
+    public interface IService<T> 
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> AddItem(T item);
+        Task<T> UpdateItem(int id, T item);
+        Task DeleteItem(int id);
+    }
+}
