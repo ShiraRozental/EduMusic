0f2697d01c3d96ce53b349423ab43d0b73ecfc58
Shira Rozental <shira0583241016@gmail.com>
2026-02-24 18:09:43 +0200

    feat: add repository interfaces

---
+++ Repository/Interfaces/IAdminRepository.cs
@@ -0,0 +1,14 @@
+﻿using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface IAdminRepository: IRepository<Admin>
+    {
+        Task<Admin?> GetAdminByEmail(string email);
+    }
+}
---
+++ Repository/Interfaces/ISongRepository.cs
@@ -0,0 +1,19 @@
+﻿using Repository.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interfaces
+{
+    public interface ISongRepository: IRepository<Song>
+    {
+        Task<Song> GetSongWithDetails(int id);
+        Task<List<Song>> SearchSongs(string query);
+
+        Task<List<Song>> GetSongsByCategory(int categoryId);
+
+
+    }
+}
